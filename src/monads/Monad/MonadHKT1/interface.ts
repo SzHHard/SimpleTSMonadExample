499d8ae509093fec32f77f4c15c2df28d9b5e7bc
Mikhail Korenkov <mg.korenkov@gmail.com>
2024-09-08 17:37:17 +0300

    rewritten with classes

--- src/monads/Monad/MonadHKT1/interface.ts
+++ src/monads/Monad/MonadHKT1/interface.ts
@@ -1,5 +1,5 @@
 interface URItoKind1<A> { // simple HKT
-    'Option': Option<A>
+    'Maybe': Maybe<A>
 }
 type URIS1 = keyof URItoKind1<unknown>
 type Kind1<F extends URIS1, A> = URItoKind1<A>[F]
@@ -9,28 +9,60 @@ interface Monad1<M extends URIS1, A> {
 }
 
 
-// type Option<A> = Monad<'Option', A>
-export interface Option<A> extends Monad1<'Option', A> {
-    get: () => A
-    getOrDefault: (defaultValue: A) => A
+// type Maybe<A> = Monad<'Maybe', A>
+export abstract class Maybe<A> implements Monad1<'Maybe', A> {
+    abstract get(): A | null
+    abstract getOrDefault(defaultValue: A): A
+    abstract flatMap<B>(f: (a: A) => Maybe<B>): Maybe<B>;
+
+    static of<I>(value: I): Maybe<I> {
+        if (typeof value !== 'undefined' && value !== null) {
+            return new Some<I>(value)
+        }
+        return new None()
+    }
+
 }
 
 
-export function Some<A>(value: A): Option<A> {
-    return {
-        get: () => value,
-        getOrDefault: () => value,
-        flatMap: f => f(value),
-    };
+export class Some<A> extends Maybe<A> {
+    private readonly value: A
+
+    constructor(value: A) {
+        super()
+        this.value = value
+    }
+
+    get() {
+        return this.value
+    }
+    getOrDefault() {
+        return this.value
+    }
+    flatMap<B>(f: (a: A) => Maybe<B>): Maybe<B> {
+        return f(this.value)
+    }
+
 }
 
-export function None<A>(): Option<A> {
-    return {
-        get: () => <A>null, // TODO: error
-        getOrDefault: (defaultValue: A) => defaultValue,
-        flatMap: f => None(),
-    };
+export class None<A> extends Maybe<A> {
+
+    constructor() {
+        super()
+
+    }
+    get(): null {
+        return null
+    }
+    getOrDefault(defaultValue: A) {
+        return defaultValue
+    }
+    flatMap<B>(f: (a: A) => Maybe<B>): Maybe<B> {
+        return new None()
+    }
 }
+
+
 
 
 
--- src/monads/testMonads.ts
+++ src/monads/testMonads.ts
@@ -1,36 +1,36 @@
 import { describe, it } from "node:test"
 
 import assert from "node:assert"
-import { None, Option, Some } from "./Monad/MonadHKT1/interface"
+import { None, Maybe, Some } from "./Monad/MonadHKT1/interface"
 
 
 export const testMonads = () => {
-    function divide(devident: number, devisor: number): Option<number> {
-        return devisor === 0 ? None() : Some(devident / devisor)
+    function divide(devident: number, devisor: number): Maybe<number> {
+        return devisor === 0 ? new None() : new Some(devident / devisor)
     }
 
     describe('Check monadic laws', () => {
-        const f = (val: number) => Some(val * 2)
+        const f = (val: number) => new Some(val * 2)
 
         it('should follow left-identity', () => {
             const start = 1
-            assert.equal(Some(start).flatMap(f).get(), Some(start * 2).get())
+            assert.equal(new Some(start).flatMap(f).get(), new Some(start * 2).get())
         })
         it('should follow right-identity', () => {
             const start = 2
-            assert.equal(Some(start).flatMap(f).get(), Some(start * 2).get())
+            assert.equal(new Some(start).flatMap(f).get(), new Some(start * 2).get())
         })
         it('should be assosiative', () => {
 
-            const first = Some(3).flatMap(val => (Some(val + 2).flatMap(f))).get()
-            const second = Some(3).flatMap(val => Some(val + 2)).flatMap(f).get()
+            const first = new Some(3).flatMap(val => (new Some(val + 2).flatMap(f))).get()
+            const second = new Some(3).flatMap(val => new Some(val + 2)).flatMap(f).get()
             assert.equal(first, second)
         })
     })
 
 
-    console.log(divide(10, 0).flatMap(res => Some(String(res))).get())
-    console.log(divide(10, 2).flatMap(res => Some(String(res))).get())
+    console.log(divide(10, 0).flatMap(res => new Some(String(res))).get())
+    console.log(divide(10, 2).flatMap(res => new Some(String(res))).get())
 }
 
 
